deb34f295dc91a758984fb23ceebcfe28dc8c371
Jim Maas <jimmaasuk@gmail.com>
2015-07-02 17:19:02 +0100

    update site 02/07/15  5:19:02 pm BST

--- publish.sh
+++ publish.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-## Script modified from
-## https://github.com/cboettig/labnotebook/blob/master/publish.sh by JAM on
-## 2/7/2015
+## Script modified from https://github.com/cboettig/labnotebook/blob/master/publish.sh 
+
+# # check if any .Rmd files in _posts. if yes, knit to .md
+# cd _posts
+# if [[ -n `ls | grep Rmd$` ]]	# 
+# then
+#     # convert all Rmarkdown (Rmd) posts to md
+#     for f in ./*.Rmd
+#     do 
+#         echo "Processing $f to md"
+#         knit -n $f # Rmd to md only, no html generated
+#         mv $f _Rmd_cache/. # move Rmd file so only md file is used by jekyll. Rmd_cache should be 
+#                       # excluded in _config.yml
+#         echo "Moved $f to Rmd_cache"
+#     done
+# fi
+
+#cd .. # back to root directory
 
 ## Compile the site
 jekyll build
@@ -15,6 +30,7 @@ git push rstat.consulting master
 echo "Site updated $(date +"%x %r") on Github server"
 
 ## Send site to Amazon S3 for web hosting
-s3cmd sync --delete-removed _site/ s3://rstat.consulting
+## s3cmd sync --delete-removed _site/ s3://rstat.consulting
+s3cmd sync _site/ s3://jamaas.com
 echo "Site updated $(date +"%x %r") on Amazon S3"
 
